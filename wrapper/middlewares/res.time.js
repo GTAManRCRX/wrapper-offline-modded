ded5f830410bab16cac772a2476a84749af41f53
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-04 17:59:43 +0100

    Fájlok eltávolítása a távoli repóból, helyi példány megtartva

--- wrapper/middlewares/res.time.js
+++
@@ -1,8 +0,0 @@
-const httpz = require("@octanuary/httpz")
-
-module.exports = async function(req, res, next) {
-	const start = Date.now();
-	await next();
-	const duration = Date.now() - start;
-	console.log(`${req.method} ${req.url} - ${res.statusCode} ${duration}ms`);
-};
