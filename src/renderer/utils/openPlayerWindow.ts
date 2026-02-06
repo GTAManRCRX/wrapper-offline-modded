6aae6d42c91fe9056a5cd41686a1ef50e1722fa3
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-06 17:08:20 +0100

    Video player now unresizable

--- src/renderer/utils/openPlayerWindow.ts
+++ src/renderer/utils/openPlayerWindow.ts
@@ -1,6 +1,6 @@
 export default function openPlayerWindow(movieId: string) {
     const isWideRaw = localStorage.getItem("isWide_active");
-    const isWide = isWideRaw === "true" || isWideRaw === "1";
+    const isWide = isWideRaw === null ? true : (isWideRaw === "true" || isWideRaw === "1");
 
     const baseWidth = 1280;
     const innerHeight = isWide ? 720 : 823;
@@ -17,6 +17,6 @@ export default function openPlayerWindow(movieId: string) {
     window.open(
         `?redirect=/movies/play/${movieId}`,
         "Video player - Wrapper offline",
-        `width=${width},height=${height},left=${left},top=${top},menubar=no,status=no,resizable=yes`
+        `width=${width},height=${height},left=${left},top=${top},menubar=no,status=no,resizable=no`
     );
 }
