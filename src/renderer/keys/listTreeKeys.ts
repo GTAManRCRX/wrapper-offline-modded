d94ebada39d22cb7861486dca6d193dcfd4b3f3d
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-06 14:36:46 +0100

    Code cleanup

--- src/renderer/keys/listTreeKeys.ts
+++ src/renderer/keys/listTreeKeys.ts
@@ -1,5 +1,5 @@
 import { InjectionKey } from "vue";
 
 const columnIdKey = Symbol();
-/** list of list entry column ids to display */
+
 export const genericColumnIdKey = <T>() => columnIdKey as InjectionKey<(keyof T)[]>;
