d54f3cb081123edbc398950e17e28bfd33bf26fc
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-06 14:35:21 +0100

    Code cleanup

--- src/renderer/interfaces/Asset.ts
+++ src/renderer/interfaces/Asset.ts
@@ -3,12 +3,12 @@ export interface Asset {
 	title: string,
 	type: string,
 	subtype: string,
-	/** prop type, if the asset is a prop */
+
 	ptype: string,
-	/** duration in ms for sound assets */
+
 	duration: number,
-	/** width in px for video props */
+
 	width: number,
-	/** height in px for video props */
+
 	height: number,
 };
